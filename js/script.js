8a869b7ae4f4875da11f1cc6702f740621135e7e
halo2008 <ksedkowski@gmail.com>
2017-05-07 13:12:12 +0200

    drobna poprawka

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
 var animal = 'zielone słonie';
-
 var animal = animal.toUpperCase();
-var text = text.replace('Papugi', animal);
-
+var textChange = text.replace('Papugi', animal);
 var partOFtext = text.slice(0, text.length/2);
 
 console.log(partOFtext);
